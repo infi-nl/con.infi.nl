0b9a9fb565c39e34f8a5ee891831d10eeda95087
Bas van Doren <bas.van.doren@infi.nl>
2026-04-03 13:53:23 +0000

    Add CC0 icons to socials

---
+++ src/lib/components/SimpleIcon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  interface Props {
+    icon: { path: string; title: string };
+    class?: string;
+  }
+
+  let { icon, class: className = 'inline-block w-5 h-5' }: Props = $props();
+</script>
+
+<svg role="img" viewBox="0 0 24 24" fill="currentColor" aria-label={icon.title} class={className}><path d={icon.path} /></svg>
--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -1,13 +1,31 @@
 <script>
   import A from '$lib/components/A.svelte';
+  import SimpleIcon from '$lib/components/SimpleIcon.svelte';
+  import { siRss, siMastodon, siBluesky, siInstagram, siX, siGithub } from 'simple-icons';
 </script>
 
 <div class="flex flex-wrap gap-4 justify-center">
-  <div><A clazz="bg-black/10 p-8 inline-block" href="https://infi.nl/blog">Ons blog</A></div>
-  <div><A clazz="bg-black/10 p-8 inline-block" href="https://mastodon.social/@infinl@mastodon.nl">Mastodon</A></div>
-  <div><A clazz="bg-black/10 p-8 inline-block" href="https://bsky.app/profile/infi.nl">Bluesky</A></div>
-  <div><A clazz="bg-black/10 p-8 inline-block" href="https://www.instagram.com/infi.nl">Instagram</A></div>
-  <div><A clazz="bg-black/10 p-8 inline-block" href="https://twitter.com/infinl">X / Twitter</A></div>
+  <div><A clazz="bg-black/10 p-8 inline-flex items-center gap-2" href="https://infi.nl/blog"><SimpleIcon icon={siRss} />Ons blog</A></div>
+  <div>
+    <A clazz="bg-black/10 p-8 inline-flex items-center gap-2" href="https://mastodon.social/@infinl@mastodon.nl"
+      ><SimpleIcon icon={siMastodon} /> Mastodon</A
+    >
+  </div>
+  <div>
+    <A clazz="bg-black/10 p-8 inline-flex items-center gap-2" href="https://bsky.app/profile/infi.nl"
+      ><SimpleIcon icon={siBluesky} /> Bluesky</A
+    >
+  </div>
+  <div>
+    <A clazz="bg-black/10 p-8 inline-flex items-center gap-2" href="https://www.instagram.com/infi.nl"
+      ><SimpleIcon icon={siInstagram} /> Instagram</A
+    >
+  </div>
+  <div>
+    <A clazz="bg-black/10 p-8 inline-flex items-center gap-2" href="https://twitter.com/infinl"><SimpleIcon icon={siX} /> X / Twitter</A>
+  </div>
   <div><A clazz="bg-black/10 p-8 inline-block" href="https://www.linkedin.com/company/infinl">LinkedIn</A></div>
-  <div><A clazz="bg-black/10 p-8 inline-block" href="https://github.com/infi-nl">GitHub</A></div>
+  <div>
+    <A clazz="bg-black/10 p-8 inline-flex items-center gap-2" href="https://github.com/infi-nl"><SimpleIcon icon={siGithub} /> GitHub</A>
+  </div>
 </div>

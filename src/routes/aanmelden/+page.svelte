623b877274ca8785188e0f1264e9c98faca6cc77
Jeroen Heijmans <github@jeroenheijmans.nl>
2026-06-08 18:07:45 +0200

    Last minute tweaks (#28)
    * Programma-switch
    
    * Prepare feedback form
    
    * Aanmelden pagina goed (genoeg) voor vlak voor- en na het event
    
    * Fix times after reshuffle

--- src/routes/aanmelden/+page.svelte
+++ src/routes/aanmelden/+page.svelte
@@ -1,28 +1,43 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import A from '$lib/components/A.svelte';
+  import Socials from '$lib/components/Socials.svelte';
   import SimplePageContent from '$lib/components/SimplePageContent.svelte';
   import SimplePageHeader from '$lib/components/SimplePageHeader.svelte';
+
+  // UTC voor simpelheid, het luistert ook weer niet zo nauw.
+  const cutoffTimestamp = Date.UTC(2026, 5, 12);
+  let showPostEventContent = false;
+  onMount(() => {
+    showPostEventContent = Date.now() >= cutoffTimestamp;
+  });
 </script>
 
 <SimplePageHeader>Aanmelden Infi-Con 2026</SimplePageHeader>
 
 <SimplePageContent>
-  <p>
-    De <strong>2026 editie</strong> zal plaatsvinden op <strong>donderdag 11 juni</strong>
-    van <strong>12:30 tot 17:00</strong> (<A href="/2026/programma">programma</A>) op kantoor bij Infi (Arthur van Schendelstraat 500, 3511
-    MH, Utrecht).
-  </p>
-  <p>Aanmelden kan via onderstaand formulier:</p>
-  <a
-    href="https://forms.office.com/e/D2JhfgaCCP"
-    rel="noreferer"
-    target="_blank"
-    class="bg-black/10 hover:bg-black/20 border border-black/30 rounded-lg h-64 w-full flex items-center justify-center text-orange-600 no-underline"
-    >Ga naar het aanmeldformulier...</a
-  >
-  <p>
-    En: fijn als je jouw RSVP up to date houdt. Aangemeld, maar kun je toch echt niet komen? Stuur dan een kort mailtje naar <strong
-      ><a href="mailto:community@infi.nl">community@infi.nl</a></strong
-    >
-  </p>
+  {#if showPostEventContent}
+    <p>
+      De <strong>2026 editie</strong> was op <strong>donderdag 11 juni</strong>
+      en is helaas alweer voorbij! Je kunt nog terugkijken wat het <A href="/2026/programma">programma</A> was. Volg ons op socials om een seintje
+      te krijgen voor volgende edities.
+    </p>
+    <Socials />
+  {:else}
+    <p>
+      De <strong>2026 editie</strong> zal plaatsvinden op <strong>donderdag 11 juni</strong>
+      van <strong>12:30 tot 17:00</strong> (<A href="/2026/programma">programma</A>) op kantoor bij Infi (Arthur van Schendelstraat 500,
+      3511 MH, Utrecht).
+    </p>
+    <p>
+      <strong>Aanmelden kan ook last-minute nog!</strong> Het formulier is gesloten maar je kunt ons gewoon even mailen op
+      <strong><a href="mailto:community@infi.nl">community@infi.nl</a></strong> om te laten weten dat je er ook bij gaat zijn. Dan weten we waar
+      we rekening mee kunnen houden qua aantallen.
+    </p>
+    <p>
+      Als je al eerder was aangemeld: fijn als je jouw RSVP up to date houdt. Kun je toch echt niet komen? Stuur dan een kort mailtje naar <strong
+        ><a href="mailto:community@infi.nl">community@infi.nl</a></strong
+      > om het ons te laten weten.
+    </p>
+  {/if}
 </SimplePageContent>
